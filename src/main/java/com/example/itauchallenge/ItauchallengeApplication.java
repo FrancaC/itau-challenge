347e15b0287ff14dadd94c60c715990416436853
FrancaC <clebesonfranca@gmail.com>
2024-09-07 08:57:14 -0300

    [Settings] - configuração inicial

---
+++ src/main/java/com/example/itauchallenge/ItauchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.itauchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itauchallenge/ItauchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itauchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
